b871710ba010d31231055ef327cfa734adb211fa
EwyBoy <ewyboy@gmail.com>
2019-03-20 22:51:35 +0100

    Update 1.1.0
    Moved champion database out of script into separate json file

--- src/opgg.py
+++ src/opgg.py
@@ -1,9 +1,11 @@
 import webbrowser
 import readline
+import json
 
 
-# VERSION: 1.0.1 #
+# VERSION: 1.1.0 #
 # Made by Ewy 16.10.2018 #
+# Last Update: 20.03.2019 #
 
 class TabCompleter(object):
 
@@ -24,23 +26,6 @@ class TabCompleter(object):
             return None
 
 
-champlist = [
-    "aatrox", "ahri", "akali", "alistar", "amumu", "anivia", "annie", "ashe", "aurelion sol", "azir", "bard",
-    "blitzcrank", "brand", "braum", "caitlyn", "camille", "cassiopeia", "chogath", "corki", "darius", "diana",
-    "drmundo", "draven", "ekko", "elise", "evelynn", "ezreal", "fiddlesticks", "fiora", "fizz", "galio",
-    "gangplank", "garen", "gnar", "gragas", "graves", "hecarim", "heimerdinger", "illaoi", "ivern", "irelia",
-    "janna", "jarvaniv", "jax", "jayce", "jhin", "jinx", "kaisa", "kalista", "karma", "karthus", "kassadin",
-    "katarina", "kayle", "kayn", "kennen", "khazix", "kindred", "kled", "kogmaw", "leblanc", "leesin", "leona",
-    "lissandra", "lucian", "lulu", "lux", "malphite", "malzahar", "maokai", "masteryi", "miss fortune",
-    "mordekaiser", "morgana", "nami", "nasus", "nautilus", "nidalee", "nocturne", "nunu", "olaf", "orianna",
-    "ornn", "pantheon", "poppy", "pyke", "quinn", "rakan", "rammus", "reksai", "renekton", "rengar", "riven",
-    "rumble", "ryze", "sejuani", "shaco", "shen", "shyvana", "singed", "sion", "sivir", "skarner", "sona",
-    "soraka", "swain", "syndra", "tahmkench", "taliyah", "talon", "taric", "teemo", "thresh", "tristana",
-    "trundle", "tryndamere", "twistedfate", "twitch", "udyr", "urgot", "varus", "vayne", "veigar", "velkoz",
-    "vi", "viktor", "vladimir", "volibear", "warwick", "wukong", "xayah", "xerath", "xinzhao", "yasuo",
-    "yorick", "zac", "zed", "ziggs", "zilean", "zoe"
-]
-
 lanelist = ["mid", "top", "jungle", "bot", "support"]
 
 
@@ -53,7 +38,9 @@ def getTabCompletion(list):
 
 
 def getChamp():
-    getTabCompletion(champlist)
+    champions = open('champions.json')
+    champion = json.load(champions)
+    getTabCompletion(champion)
     global champ
     champ = raw_input('Champion: ')
     return champ
